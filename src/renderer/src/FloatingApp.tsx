5e0c5b6e69d4a938f23882bf1166dec0dc3d01ea
ezequielnick <107352853+ezequielnick@users.noreply.github.com>
2025-08-09 09:48:09 +0800

    fix: change floating window to rectangle to avoid white border issue

--- src/renderer/src/FloatingApp.tsx
+++ src/renderer/src/FloatingApp.tsx
@@ -60,7 +60,7 @@ const FloatingApp: React.FC = () => {
 
   return (
     <div className="app-drag h-screen w-screen overflow-hidden">
-      <div className="floating-bg border border-divider flex rounded-full bg-content1 h-[calc(100%-2px)] w-[calc(100%-2px)]">
+      <div className="floating-bg border border-divider flex bg-content1 h-full w-full">
         <div className="flex justify-center items-center h-full aspect-square">
           <div
             onContextMenu={(e) => {
@@ -78,7 +78,7 @@ const FloatingApp: React.FC = () => {
                   }
                 : {}
             }
-            className={`app-nodrag cursor-pointer floating-thumb ${tunEnabled ? 'bg-secondary' : sysProxyEnabled ? 'bg-primary' : 'bg-default'} hover:opacity-hover rounded-full h-[calc(100%-4px)] aspect-square`}
+            className={`app-nodrag cursor-pointer floating-thumb ${tunEnabled ? 'bg-secondary' : sysProxyEnabled ? 'bg-primary' : 'bg-default'} hover:opacity-hover h-[calc(100%-4px)] aspect-square`}
           >
             <MihomoIcon className="floating-icon text-primary-foreground h-full leading-full text-[22px] mx-auto" />
           </div>
