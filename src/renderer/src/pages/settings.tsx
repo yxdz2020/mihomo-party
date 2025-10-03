a7de9b2588e133de00a4ffd176f4b1b1915698c1
Memory <134070804+Memory2314@users.noreply.github.com>
2025-10-03 22:49:41 +0800

    refactor: Redesign and migrate WebUI to global settings

--- src/renderer/src/pages/settings.tsx
+++ src/renderer/src/pages/settings.tsx
@@ -10,6 +10,7 @@ import ShortcutConfig from '@renderer/components/settings/shortcut-config'
 import { FaTelegramPlane } from 'react-icons/fa'
 import SiderConfig from '@renderer/components/settings/sider-config'
 import SubStoreConfig from '@renderer/components/settings/substore-config'
+import WebUIConfig from '@renderer/components/settings/webui-config'
 import { useTranslation } from 'react-i18next'
 
 const Settings: React.FC = () => {
@@ -60,6 +61,7 @@ const Settings: React.FC = () => {
       }
     >
       <GeneralConfig />
+      <WebUIConfig />
       <SubStoreConfig />
       <SiderConfig />
       <WebdavConfig />
