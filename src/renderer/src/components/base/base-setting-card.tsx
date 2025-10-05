b1d02c9f82e20e3bbc052a22519b189a81550a7e
ezequielnick <107352853+ezequielnick@users.noreply.github.com>
2025-10-05 11:43:24 +0800

    style: add indicator icons to expandable items in settings page

--- src/renderer/src/components/base/base-setting-card.tsx
+++ src/renderer/src/components/base/base-setting-card.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Accordion, AccordionItem, Card, CardBody } from '@heroui/react'
+import { IoIosArrowBack } from 'react-icons/io'
 
 interface Props {
   title?: string
@@ -8,17 +9,35 @@ interface Props {
 }
 
 const SettingCard: React.FC<Props> = (props) => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return !props.title ? (
     <Card className={`${props.className} m-2`}>
       <CardBody>{props.children}</CardBody>
     </Card>
   ) : (
-    <Accordion isCompact className={`${props.className} my-2`} variant="splitted" {...props}>
+    <Accordion
+      isCompact
+      className={`${props.className} my-2`}
+      variant="splitted"
+      onSelectionChange={(keys) => {
+        setIsOpen(keys instanceof Set ? keys.size > 0 : false)
+      }}
+      {...props}
+    >
       <AccordionItem
         className="data-[open=true]:pb-2"
         hideIndicator
         keepContentMounted
-        title={props.title}
+        textValue={props.title}
+        title={
+          <div className="flex justify-between items-center w-full">
+            <span>{props.title}</span>
+            <IoIosArrowBack
+              className={`transition duration-200 ml-2 h-[32px] text-lg text-foreground-500 ${isOpen ? '-rotate-90' : ''}`}
+            />
+          </div>
+        }
       >
         {props.children}
       </AccordionItem>
